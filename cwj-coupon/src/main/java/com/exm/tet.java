2f38d467ccace9c9fbfa3d4f521fad003055f79d
baope <m13537625866@163.com>
2023-11-12 23:38:27 +0800

    try

---
+++ cwj-coupon/src/main/java/com/exm/tet.java
@@ -0,0 +1,12 @@
+package com.exm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tet {
+    public static void main(String[] args) {
+        SpringApplication.run(tet.class,args);
+    }
+}
